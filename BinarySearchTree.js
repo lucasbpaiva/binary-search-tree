d0bc4c563957e600acf71bac476493c26b7491a5
Lucas Paiva <lpaiva234@gmail.com>
2025-12-04 12:23:53 -0300

    implement buildTree and sortedArrayToBST methods

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -10,4 +10,23 @@
     constructor(array) {
         this.root = this.buildTree(array);
     }
+
+    buildTree(array) {
+        // converts array to a height-balanced binary search tree
+        const sortedUniqueArray = [...new Set(originalArray)].sort((a, b) => a - b);
+        return this.sortedArrayToBST(sortedUniqueArray);
+    }
+
+    sortedArrayToBST(array) {
+        // converts sorted array without duplicates to a height-balanced binary search tree
+        if (array.length === 0) return null;
+        
+        let mid = Math.floor(array.length / 2);
+        let result = new Node(array[mid]);
+
+        result.left = this.sortedArrayToBST(array.slice(0, mid));
+        result.right = this.sortedArrayToBST(array.slice(mid + 1));
+
+        return result;
+    }
  }
