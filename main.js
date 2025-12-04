83476b6e0215e3f8cd2f217e1c305c7d1d693ce0
Lucas Paiva <lpaiva234@gmail.com>
2025-12-04 13:32:28 -0300

    add test for insertion method

---
+++ main.js
@@ -0,0 +1,11 @@
+import { Tree } from "./BinarySearchTree.js";
+
+const nums = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const myTree = new Tree(nums);
+myTree.prettyPrint(myTree.root);
+
+// test insertion
+console.log("--------------------------------------------------");
+console.log("Add new node 31:\n");
+myTree.insert(myTree.root, 31);
+myTree.prettyPrint(myTree.root);
