6fe7cc8bb1169334307da5e145f22daf517b5628
Lucas Paiva <lpaiva234@gmail.com>
2025-12-08 15:02:50 -0300

    add test for levelOrderForEach method

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
 import { Tree } from "./BinarySearchTree.js";
 
+function printValue(node) {
+    console.log(node.val);
+}
+
 const nums = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const myTree = new Tree(nums);
 myTree.prettyPrint(myTree.root);
 
 // test insertion
-console.log("--------------------------------------------------");
-console.log("Add new node 31:\n");
-myTree.insert(myTree.root, 31);
-myTree.prettyPrint(myTree.root);
+// console.log("--------------------------------------------------");
+// console.log("Add new node 31:\n");
+// myTree.insert(myTree.root, 31);
+// myTree.prettyPrint(myTree.root);
 
 //test deletion
-console.log("--------------------------------------------------");
-console.log("Delete node 8:\n");
-myTree.deleteNode(myTree.root, 8);
-myTree.prettyPrint(myTree.root);
+// console.log("--------------------------------------------------");
+// console.log("Delete node 8:\n");
+// myTree.deleteNode(myTree.root, 8);
+// myTree.prettyPrint(myTree.root);
 
 //test search
+// console.log("--------------------------------------------------");
+// console.log("Find node 67:\n");
+// myTree.prettyPrint(myTree.find(myTree.root, 67));
+
+//test levelOrderForeach
 console.log("--------------------------------------------------");
-console.log("Find node 67:\n");
-myTree.prettyPrint(myTree.find(myTree.root, 67));
+console.log("Print value of every node:\n");
+myTree.levelOrderForEach(printValue);
